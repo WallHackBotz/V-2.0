a06617c05fd043caddbe43e27ebd7a95daf38df5
Rrsszxx <96643671+WallHackBotz@users.noreply.github.com>
2022-04-13 13:44:49 +0700

    Update syas.js

--- plugins/syas.js
+++ plugins/syas.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(`https://i.ibb.co/rkTg7B0/donasi.jpg`)).buffer(), `
 ┌〔 𝘿𝙤𝙣𝙖𝙨𝙞 • 𝙀𝙢𝙤𝙣𝙚𝙮 〕
 ├ _GOPAY_ : *0896-4019-28948*
-├ _SAWERIA_ : *https://saweria.com/Alanadi*
+├ _SAWERIA_ : *https://saweria.com/Rrsszxx*
 ├ *SELAIN GOPAY VIA QR AJA ATAU SAWERIA*
 └────
 `.trim(), footer, 'Owner', '.owner')
